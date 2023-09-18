cb162b96142f455849fae2fd7496127b7325649f
bicho1717 <143564077+bicho1717@users.noreply.github.com>
2023-09-18 16:24:36 -0700

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_identificadores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // DECLARACION DE VARIABLES
+        String nombre;
+        String apellidos;
+        Scanner capturota = new Scanner(System.in);
+         // CAPTURAR DE VARIABLES
+         System.out.println("Introduce tu nombre");
+         nombre = capturota.nextLine();
+         System.out.println("Introduce tu apelllido");
+         apellidos = capturota.nextLine();
+         // JAVA ES SENSIBLE CON LAS MAYUSCULAS
+    }
+    
+}
